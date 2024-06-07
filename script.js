1a09bb4b42258203062e26fe63c883a266255242
Mr.TEN <34998996+vBathuN@users.noreply.github.com>
2024-06-07 08:18:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//<![CDATA[
+$(document).ready(function () {
+Recentpostbsd({
+blogURL:"https://ilaingarindia.blogspot.com/",
+MaxPost:8,
+container:"#recentpostbsd",
+ImageSize:500,
+Time:9000,
+autoplay:true,
+tagName:false
+});
+});
+//]]>
