4e06ef1bc63ab09527af9389a4376590d0ff7cf6
Mr.TEN <34998996+vBathuN@users.noreply.github.com>
2024-06-07 16:55:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //<![CDATA[
 $(document).ready(function () {
     pengaturan({
-        blogURL: 'https://www.bloggerszones.com/',
+        blogURL: 'https://debatedais.blogspot.com/',
         MaxPost: 5,
         RandompostActive: false,
         ImageSize: 620,
-        NumCharacter: 150,
-        idcontaint: "#skitterslideshow",
+        NumCharacter: 50,
+        idcontaint: "#slideshow",
         cmtext: "Comment",
         NoCmtext: "No Comment Bro!",
         pBlank: "https://2.bp.blogspot.com/-D47WWjFZXdk/UP1z-A5uCuI/AAAAAAAAH98/heTBvxq49No/s1600/noimage.jpg",
