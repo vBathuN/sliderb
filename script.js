1c561c373e0c3b3f5c8d9ad8be173f7cf043b04a
Mr.TEN <34998996+vBathuN@users.noreply.github.com>
2024-06-07 16:53:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//<![CDATA[
+$(document).ready(function () {
+    pengaturan({
+        blogURL: 'https://www.bloggerszones.com/',
+        MaxPost: 5,
+        RandompostActive: false,
+        ImageSize: 620,
+        NumCharacter: 150,
+        idcontaint: "#skitterslideshow",
+        cmtext: "Comment",
+        NoCmtext: "No Comment Bro!",
+        pBlank: "https://2.bp.blogspot.com/-D47WWjFZXdk/UP1z-A5uCuI/AAAAAAAAH98/heTBvxq49No/s1600/noimage.jpg",
+        tagName: false
+    });
+});
+//]]>
